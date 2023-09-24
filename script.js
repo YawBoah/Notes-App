f6b9d7c077c9ecebc0ed5e7c134b58ab088c1c1d
YBoah <yawboahene.yb@gmail.com>
2023-09-24 18:45:24 +0000

    Arrangement of the codes

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+
 function showNotes(){
     notesContainer.innerHTML = localStorage.getItem("notes");
 }
 showNotes();
 
+
 function updateStorage() {
     localStorage.setItem("notes", notesContainer.innerHTML);
 }
+
 
 createBtn.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
