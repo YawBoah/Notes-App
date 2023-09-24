e924202ec32c3a77462a7a4a74cb6678df30bed5
YBoah <yawboahene.yb@gmail.com>
2023-09-24 18:44:41 +0000

    Did a correct arragement of the coding

--- script.js
+++ script.js
@@ -38,6 +38,6 @@ notesContainer.addEventListener("click", function(e){
 document.addEventListener("keydown", event => {
     if(event.key === "Enter"){
         document.execCommand("insertLineBreak");
-        event.preventDefault();
+        event.preventDefault(); 
     }
 })
