14ea83c07436572f5e1fd2878875a4bd216f6b6c
YBoah <yawboahene.yb@gmail.com>
2023-09-24 17:32:24 +0000

    Added the input functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+createBtn.addEventListener("click", ()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
