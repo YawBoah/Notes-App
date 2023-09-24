1c2531d35fa72c134b8bc8a6d94777157f2a212d
YBoah <yawboahene.yb@gmail.com>
2023-09-24 17:58:14 +0000

    Added the Enter functionality

--- script.js
+++ script.js
@@ -34,3 +34,10 @@ notesContainer.addEventListener("click", function(e){
         })
     }
 })
+
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
